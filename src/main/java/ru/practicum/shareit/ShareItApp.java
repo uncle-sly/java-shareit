518c2519748a6ac65c3c658434a76bba2870ad0d
u-sly <150694587+u-sly@users.noreply.github.com>
2024-11-21 01:21:19 +0300

    Создание структуры проекта: пакеты, интерфейсы, классы.

--- src/main/java/ru/practicum/shareit/ShareItApp.java
+++ src/main/java/ru/practicum/shareit/ShareItApp.java
@@ -1,13 +1,16 @@
 package ru.practicum.shareit;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class ShareItApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShareItApp.class, args);
+		log.info("Current working directory: " + System.getProperty("user.dir"));
 	}
 
 }
---
+++ src/main/java/ru/practicum/shareit/item/repository/InMemoryItemRepositoryImpl.java
@@ -0,0 +1,4 @@
+package ru.practicum.shareit.item.repository;
+
+public class InMemoryItemRepositoryImpl implements ItemRepository{
+}
